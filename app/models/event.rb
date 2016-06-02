96634c2a6d7461a5e747d65559f2d7894e33cb48
Alexi Carey <ciegoproductions@gmail.com>
2016-06-02 12:01:18 -0600

    add logger and error handling to event import method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,32 +1,65 @@
 class Event < ActiveRecord::Base
 
-  def self.import_pechange_events
-    pechanga_id = 136
-    wiki_event = WikiEvent.new "https://www.pechanga.com/", "rest/v2.0/entertainment/api/Entertainment/E?_=1463671134566"
+  def self.import_pechanga_events
+    begin
+      events_processed, events_imported = 0, 0
 
-    events_array = wiki_event.parse_pechanga_rest_api
+      pechanga_id = 136
+      event_update_logger.info("Starting Pechanga Event Import - venue_id #{pechanga_id}")
 
-    events_array.each do |event_hash|
+      wiki_event = WikiEvent.new "https://www.pechanga.com/", "rest/v2.0/entertainment/api/Entertainment/E?_=1463671134566"
+      events_array = wiki_event.parse_pechanga_rest_api
 
-      if event_hash && event_hash['title']
+      event_update_logger.info("    #{events_array.length} Events Found")
 
-        dates = event_hash['dates']
-        new_dates = dates.split(/([pm|am],)/).each_slice(2).map(&:join)
+      events_array.each do |event_hash|
 
-        new_dates.each do |date|
-          start_date = date.to_datetime.to_formatted_s(:db)
+        if event_hash && event_hash['title']
 
-          event = Event.find_or_initialize_by(venue_id: pechanga_id, title: event_hash['title'], start_date: start_date)
 
-          event.update_attributes(
-              description: event_hash['description'],
-              end_date: event_hash['end_date'],
-              link_url: event_hash['link_url'],
-              image_url: event_hash['image_url'],
-          )
-        end unless new_dates.empty?
-      end
+          dates = event_hash['dates']
+          new_dates = dates.split(/([pm|am],)/).each_slice(2).map(&:join)
 
-    end unless events_array.empty?
+          event_update_logger.info("    Parsing #{event_hash['title']} Event with #{new_dates.length} different start_dates")
+
+          new_dates.each do |date|
+            start_date = date.to_datetime.to_formatted_s(:db)
+
+            event_update_logger.info("    Loading Event Start Date: #{start_date}")
+
+            event = Event.find_or_initialize_by(venue_id: pechanga_id, title: event_hash['title'], start_date: start_date)
+
+            if event.new_record?
+              event_update_logger.info("  New Event - Title #{event_hash['title']}")
+              events_imported += 1
+            end
+
+            event.update_attributes(
+                description: event_hash['description'],
+                end_date: event_hash['end_date'],
+                link_url: event_hash['link_url'],
+                image_url: event_hash['image_url'],
+            )
+            events_processed += 1
+          end unless new_dates.empty?
+        end
+
+      end unless events_array.empty?
+
+        event_update_logger.info("  Events Processed: #{events_processed}")
+        event_update_logger.info("  New Events Imported: #{events_imported}")
+    rescue Exception => e
+      event_update_logger.error(e.inspect)
+      #raise
+    end
   end
+
+  #
+  def self.event_update_logger
+    @event_update_logs ||= Logger.new("#{Rails.root}/log/event_updates.log")
+  end
+  def event_update_logger
+    @event_update_logs ||= Logger.new("#{Rails.root}/log/event_updates.log")
+  end
+
 end
